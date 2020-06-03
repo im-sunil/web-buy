fa4be65fc7e10d78fc42166cf654b0d3857adf51
Sunil Bagde <bagdesunil786@gmail.com>
2020-06-04 02:59:54 +0530

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,4 +12,13 @@ require_once base_path('/bootstrap/container.php');
 
 $router = $container->get('router');
 
+$router->middleware(new Tuupola\Middleware\CorsMiddleware([
+    'origin' => ['*'],
+    'methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    'headers.allow' => [],
+    'headers.expose' => [],
+    'credentials' => false,
+    'cache' => 0,
+]));
+
 require_once base_path('routes/api.php');
